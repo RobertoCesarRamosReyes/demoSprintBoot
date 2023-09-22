16560e59dee2deb8dff8615c65f9376c9d31fe35
Roberto <robertocesarramosreyes@gmail.com>
2023-09-21 22:58:59 -0600

    Se utiliza el manejo de parametros por Path con la notacion @PathVariable. El ejemplo consiste en la busqueda de jugadores por numero en un determinado equipo

--- src/main/java/com/linking/demoproject/controllers/ParametrosControllers.java
+++ src/main/java/com/linking/demoproject/controllers/ParametrosControllers.java
@@ -1,9 +1,15 @@
 package com.linking.demoproject.controllers;
 
+import com.linking.demoproject.models.Equipo;
+import com.linking.demoproject.models.Jugador;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class ParametrosControllers {
@@ -14,5 +20,36 @@ public class ParametrosControllers {
         model.addAttribute("parametro",val);
         model.addAttribute("nombre",nombre);
         return "parametros";
+    }
+
+    @GetMapping("/equipos/{nombre}/{numero}")
+    public String parametrosPorPath(@PathVariable String nombre, @PathVariable("numero") Integer numero, Model model){
+        Optional<Equipo> equipoOptional=getEquipos().stream().filter(equipo -> nombre.toLowerCase().equals(equipo.getNombre().toLowerCase())).findFirst();
+        if (equipoOptional.isPresent()){
+            Optional<Jugador> jugadorOptional=equipoOptional.get().getPlantilla().stream().filter(jugador -> numero==jugador.getNumero()).findFirst();
+            if (jugadorOptional.isPresent()){
+                model.addAttribute("jugador",jugadorOptional.get());
+            }
+        }
+        return "parametros";
+    }
+
+    private List<Equipo> getEquipos(){
+        Equipo barcelona=new Equipo();
+        barcelona.setNombre("Barcelona");
+        barcelona.addJugador(new Jugador("TER STEGEN",1));
+        barcelona.addJugador(new Jugador("ARAUJO",4));
+        barcelona.addJugador(new Jugador("BUSQUETS",5));
+        barcelona.addJugador(new Jugador("LEWANDOSKI",9));
+        barcelona.addJugador(new Jugador("DEMBELE",7));
+
+        Equipo realMadrid=new Equipo();
+        realMadrid.setNombre("RealMadrid");
+        realMadrid.addJugador(new Jugador("COURTOIS",1));
+        realMadrid.addJugador(new Jugador("CARVAJAL",2));
+        realMadrid.addJugador(new Jugador("MODRIC",10));
+        realMadrid.addJugador(new Jugador("BENZEMA",9));
+        realMadrid.addJugador(new Jugador("HAZARD",7));
+        return List.of(barcelona,realMadrid);
     }
 }
